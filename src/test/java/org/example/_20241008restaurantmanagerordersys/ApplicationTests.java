66604d4fe3852a8d35966d3f28a0309723199ff2
alfo0924 <halo0924@yahoo.com.tw>
2024-10-08 22:02:56 +0800

    v1.01 cnp

---
+++ src/test/java/org/example/_20241008restaurantmanagerordersys/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example._20241008restaurantmanagerordersys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
